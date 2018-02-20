e2d01c41fe768992d2895f152979d1924f22414e
Cyberboss <Cyberboss@users.noreply.github.com>
2018-02-20 18:14:10 -0500

    Good boy points module

---
+++ TGWebhooks/Modules/GoodBoyPoints/GoodBoyPointsEntry.cs
@@ -0,0 +1,7 @@
+﻿namespace TGWebhooks.Modules.GoodBoyPoints
+{
+    sealed class GoodBoyPointsEntry
+    {
+		public int Points { get; set; } = 10;
+    }
+}
---
+++ TGWebhooks/Modules/GoodBoyPoints/GoodBoyPointsModule.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TGWebhooks.Modules.GoodBoyPoints
+{
+	public class GoodBoyPointsModule : IModule, IPayloadHandler<PullRequestEventPayload>, IMergeRequirement
+	{
+		public Guid Uid => new Guid("a8875569-8807-4a58-adf6-ac5a408c7e16");
+
+		public string Name => "Good Boy Points";
+
+		public string Description => "Tracks user code improvement ratios";
+
+		public IEnumerable<IMergeRequirement> MergeRequirements => new List<IMergeRequirement> { this };
+
+		public IEnumerable<IMergeHook> MergeHooks => Enumerable.Empty<IMergeHook>();
+
+		static readonly IReadOnlyDictionary<string, int> LabelValues = new Dictionary<string, int>
+		{
+			{ "Fix", 2 },
+			{ "Refactor", 2 },
+			{ "CI/Tests", 3 },
+			{ "Code Improvement", 1 },
+			{ "Grammar and Formatting", 1 },
+			{ "Priority: High", 4 },
+			{ "Priority: CRITICAL", 5 },
+			{ "Logging", 1 },
+			{ "Feedback", 1 },
+			{ "Performance", 3 },
+			{ "Feature", -1 },
+			{ "Balance/Rebalance", -1 }
+		};
+
+		IDataStore dataStore;
+		IGitHubManager gitHubManager;
+		IStringLocalizer stringLocalizer;
+
+		void CheckConfigured()
+		{
+			if (dataStore == null)
+				throw new InvalidOperationException("Configure wasn't called!");
+		}
+
+		public void Configure(ILogger logger, IRepository repository, IGitHubManager gitHubManager, IIOManager ioManager, IWebRequestManager webRequestManager, IDataStore dataStore, IStringLocalizer stringLocalizer)
+		{
+			this.dataStore = dataStore;
+			this.gitHubManager = gitHubManager;
+			this.stringLocalizer = stringLocalizer;
+		}
+
+		public async Task<AutoMergeStatus> EvaluateFor(PullRequest pullRequest, CancellationToken cancellationToken)
+		{
+			CheckConfigured();
+			var userGBP = await dataStore.ReadData<GoodBoyPointsEntry>(pullRequest.User.Login, cancellationToken).ConfigureAwait(false);
+			var passed = userGBP.Points >= 0;
+			var result = new AutoMergeStatus
+			{
+				FailStatusReport = !passed,
+				Progress = userGBP.Points,
+				RequiredProgress = 0
+			};
+			if (!passed)
+				result.Notes.Add(stringLocalizer["InsufficientGBP"]);
+			return result;
+		}
+
+		public IEnumerable<IPayloadHandler<TPayload>> GetPayloadHandlers<TPayload>() where TPayload : ActivityPayload
+		{
+			CheckConfigured();
+			if (typeof(TPayload) == typeof(PullRequestEventPayload))
+				yield return (IPayloadHandler<TPayload>)this;
+		}
+
+		public Task Initialize(CancellationToken cancellationToken) => Task.CompletedTask;
+
+		public async Task ProcessPayload(PullRequestEventPayload payload, CancellationToken cancellationToken)
+		{
+			CheckConfigured();
+			if (payload.Action != "closed" || !payload.PullRequest.Merged)
+				throw new NotSupportedException();
+
+			var labelsTask = gitHubManager.GetIssueLabels(payload.PullRequest.Number);
+			var gbp = await dataStore.ReadData<GoodBoyPointsEntry>(payload.PullRequest.User.Login, cancellationToken).ConfigureAwait(false);
+
+			foreach (var L in await labelsTask.ConfigureAwait(false))
+				switch (L.Name)
+				{
+					case "PRB: No Update":
+						return;
+					case "PRB: Reset":
+						gbp = new GoodBoyPointsEntry();
+						goto exitLoop;
+					default:
+						if (LabelValues.TryGetValue(L.Name, out int award))
+							gbp.Points += award;
+						break;
+				}
+			exitLoop:
+
+			await dataStore.WriteData(payload.PullRequest.User.Login, gbp, cancellationToken);
+		}
+	}
+}
